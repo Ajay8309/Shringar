fff53c5594bffbb9a05a87e36daf1b0629798a05
Ajay8309 <ajaypanaskar8@gmail.com>
2024-01-08 14:14:06 +0530

    changes

--- middleware/unKnownEndpoint.js
+++
@@ -1,7 +0,0 @@
-const {ErrorHandler} = require("../helpers/error");
-
-const unKnownEndpoint = (Request, Response) => {
-     throw new ErrorHandler(401, "unknown endpoint");
-}
-
-module.exports = unKnownEndpoint;
--- utils/logger.js
+++
@@ -1,8 +0,0 @@
-const pino = require("pino");
-
-const logger = pino({
-    level:process.env.NODE_ENV === "production" ? "info" : "debug",
-    preetyPrint:process.env.NODE_ENV !== "production",
-});
-
-module.exports.logger = logger;
